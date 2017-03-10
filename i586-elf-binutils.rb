c6381e4dc570b6b6327d0388b7707a45edf91017
Paul Hooijenga <paul.hooijenga@sanoma.com>
2017-03-10 21:34:57 +0100

    update sha256 hashes to actually be sha256

--- i586-elf-binutils.rb
+++ i586-elf-binutils.rb
@@ -3,7 +3,7 @@ require 'formula'
 class I586ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz'
-  sha256 'f10c64e92d9c72ee428df3feaf349c4ecb2493bd'
+  sha256 'cccf377168b41a52a76f46df18feb8f7285654b3c1bd69fc8265cb0fc6902f2d'
 
   depends_on 'gcc' => :build
   def install
